8fb251b431ba0d058a2376e2eb8a40f16ee99fd2
Gareth Nicholson <gareth@io.co.za>
2020-04-28 15:51:25 +0200

    Decode content received.

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -47,6 +47,8 @@ class WebhookController extends Controller
         $authTag = hex2bin($authTagFromHeader);
         $cipherText = hex2bin($request->getContent());
 
-        return openssl_decrypt($cipherText, 'aes-256-gcm', $key, OPENSSL_RAW_DATA, $iv, $authTag);
+        $result = openssl_decrypt($cipherText, 'aes-256-gcm', $key, OPENSSL_RAW_DATA, $iv, $authTag)
+
+        return json_decode($result, true);
     }
 }
