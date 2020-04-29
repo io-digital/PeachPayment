5816de1eb56ae0f2a684495fb2df3875f560b822
Gareth Nicholson <gareth@io.co.za>
2020-04-29 11:53:39 +0200

    Add events for the different notification types.

---
+++ src/Events/RiskNotification.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace IoDigital\PeachPayment\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use IoDigital\PeachPayment\Models\PaymentEvent;
+
+class RiskNotification
+{
+    use Dispatchable, SerializesModels;
+
+    public $model;
+
+    public function __construct(PaymentEvent $event)
+    {
+        $this->model = $event;
+    }
+}
